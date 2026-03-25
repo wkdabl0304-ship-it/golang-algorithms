7c41c996ce1a8fdf3410ae8c33b397c3e95d83e2
ABL <wkdabl0304@gmail.com>
2026-03-25 16:55:54 +0800

    feat: solve LC0034 search range

---
+++ leetcode/0034_search_range.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func searchRange(nums []int, target int) []int {
+	right := search(nums, target, true) - 1
+	left := search(nums, target, false)
+	if left <= right && left >= 0 && left < len(nums) && nums[left] == target {
+		return []int{left, right}
+	}
+	return []int{-1, -1}
+}
+
+func search(nums []int, target int, isRight bool) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)>>1
+		if target > nums[mid] || (isRight && target == nums[mid]) {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return left
+}
+
+// 思路总结：二分查找如果将等于的情况并进大于或小于其中一种，相当于找左右边界了，注意一下边界就可以封装
+
+func main() {
+	nums := []int{5, 7, 7, 8, 8, 10}
+	target := 8
+	fmt.Println(searchRange(nums, target))
+}
