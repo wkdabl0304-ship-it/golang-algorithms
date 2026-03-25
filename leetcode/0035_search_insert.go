801349d5fd19e736a226eca2aea4a9d58691686c
ABL <wkdabl0304@gmail.com>
2026-03-25 11:40:29 +0800

    feat: solve LC0035 search insert

---
+++ leetcode/0035_search_insert.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func searchInsert(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			return mid
+		} else if target > nums[mid] {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return left
+}
+
+func main() {
+	nums := []int{1, 3, 5, 6}
+	target := 5
+	fmt.Println(searchInsert(nums, target))
+}
