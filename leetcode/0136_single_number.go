ddc2d6bb56adf51647e012c2299370f1c659f2e1
ABL <wkdabl0304@gmail.com>
2026-03-26 09:43:24 +0800

    feat: solve LC0136 single number

---
+++ leetcode/0136_single_number.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func singleNumber(nums []int) int {
+	res := 0
+	for _, v := range nums {
+		res ^= v
+	}
+	return res
+}
+
+func main() {
+	nums := []int{4, 1, 2, 1, 2}
+	fmt.Println(singleNumber(nums))
+}
