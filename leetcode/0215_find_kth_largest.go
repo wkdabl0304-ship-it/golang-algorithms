0a0f2486f71818a84766cdb1775d7339c9050099
ABL <wkdabl0304@gmail.com>
2026-03-30 15:50:44 +0800

    feat: solve LC0347 top k frequent

--- leetcode/0215_find_kth_largest.go
+++ leetcode/0215_find_kth_largest.go
@@ -1,46 +1,85 @@
 package main
 
 import (
+	"container/heap"
 	"fmt"
-	"math/rand"
 )
 
-// 标准解法：快排对半选择法
+// 最优解法：快排对半选择法
+//func findKthLargest(nums []int, k int) int {
+//	target := len(nums) - k // target 指的是第 k 大的数字在排序后的下标
+//	return quickSort(nums, 0, len(nums)-1, target)
+//}
+//
+//func quickSort(nums []int, left, right, target int) int {
+//	if left == right {
+//		return nums[left]
+//	}
+//	p := partition(nums, left, right)
+//	if p == target {
+//		return nums[p]
+//	} else if p < target {
+//		return quickSort(nums, p+1, right, target)
+//	} else {
+//		return quickSort(nums, left, p-1, target)
+//	}
+//}
+//
+//func partition(nums []int, left, right int) int {
+//	pivotIdx := left + rand.Intn(right-left+1)
+//	pivot := nums[pivotIdx]
+//	nums[pivotIdx], nums[right] = nums[right], nums[pivotIdx]
+//	i := left
+//	for j := left; j < right; j++ {
+//		if nums[j] < pivot {
+//			nums[i], nums[j] = nums[j], nums[i]
+//			i++
+//		}
+//	}
+//	nums[i], nums[right] = nums[right], nums[i]
+//	return i
+//}
+
+// 面试解法：小顶堆
+type minHeap0215 []int
+
+func (h *minHeap0215) Len() int {
+	return len(*h)
+}
+
+func (h *minHeap0215) Less(i, j int) bool {
+	return (*h)[i] < (*h)[j] // 下标为 i 的元素是否要排在下标为 j 的元素前面
+}
+
+func (h *minHeap0215) Swap(i, j int) {
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
+}
+
+func (h *minHeap0215) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *minHeap0215) Pop() interface{} {
+	x := (*h)[len(*h)-1]
+	*h = (*h)[:len(*h)-1]
+	return x
+}
+
 func findKthLargest(nums []int, k int) int {
-	target := len(nums) - k // target 指的是第 k 大的数字在排序后的下标
-	return quickSort(nums, 0, len(nums)-1, target)
-}
-
-func quickSort(nums []int, left, right, target int) int {
-	if left == right {
-		return nums[left]
-	}
-	p := partition(nums, left, right)
-	if p == target {
-		return nums[p]
-	} else if p < target {
-		return quickSort(nums, p+1, right, target)
-	} else {
-		return quickSort(nums, left, p-1, target)
-	}
-}
-
-func partition(nums []int, left, right int) int {
-	pivotIdx := left + rand.Intn(right-left+1)
-	pivot := nums[pivotIdx]
-	nums[pivotIdx], nums[right] = nums[right], nums[pivotIdx]
-	i := left
-	for j := left; j < right; j++ {
-		if nums[j] < pivot {
-			nums[i], nums[j] = nums[j], nums[i]
-			i++
+	h := new(minHeap0215)
+	heap.Init(h)
+	for _, v := range nums {
+		heap.Push(h, v)
+		if h.Len() > k {
+			heap.Pop(h)
 		}
 	}
-	nums[i], nums[right] = nums[right], nums[i]
-	return i
+	return (*h)[0]
 }
 
-// 思路总结：这道题难就难在对快排边界的精确理解，而且得用单向快排比较好理解
+// 思路总结：
+// 1.最优方法使用二分选择法，但是边界以及范围较难控制
+// 2.工程最优方法应该用小顶堆
 
 func main() {
 	nums := []int{3, 2, 1, 5, 6, 4}
---
+++ leetcode/0347_top_k_frequent.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"container/heap"
+	"fmt"
+)
+
+type minHeap0347 []elem0347
+
+type elem0347 struct {
+	num       int // 数字
+	frequency int // 频率
+}
+
+func (h *minHeap0347) Len() int {
+	return len(*h)
+}
+
+func (h *minHeap0347) Less(i, j int) bool {
+	return (*h)[i].frequency < (*h)[j].frequency // 下标为 i 的元素是否要排在下标为 j 的元素前面
+}
+
+func (h *minHeap0347) Swap(i, j int) {
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
+}
+
+func (h *minHeap0347) Push(x interface{}) {
+	*h = append(*h, x.(elem0347))
+}
+
+func (h *minHeap0347) Pop() interface{} {
+	x := (*h)[len(*h)-1]
+	*h = (*h)[:len(*h)-1]
+	return x
+}
+
+func topKFrequent(nums []int, k int) []int {
+	h := new(minHeap0347)
+	heap.Init(h)
+	m := make(map[int]int, len(nums))
+	var res []int
+	for _, v := range nums {
+		m[v]++
+	}
+	for key, value := range m {
+		e := elem0347{
+			num:       key,
+			frequency: value,
+		}
+		heap.Push(h, e)
+		if h.Len() > k {
+			heap.Pop(h)
+		}
+	}
+	for i := k - 1; i >= 0; i-- {
+		temp := heap.Pop(h).(elem0347)
+		res = append(res, temp.num)
+	}
+	return res
+}
+
+func main() {
+	nums := []int{1, 2, 1, 2, 1, 2, 3, 1, 3, 2}
+	k := 2
+	fmt.Println(topKFrequent(nums, k))
+}
